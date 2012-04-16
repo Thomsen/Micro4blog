5c81cdaf10e2d6fffd705d54b43f6ea39e93a5b5
androidfoross@gmail.com <androidfoross@gmail.com@ad5b3d4e-91d0-e1bb-e5e5-35cb20aa3a27>
2012-04-16 14:50:31 +0000

    footer navigation

--- trunk/src/com/micro4blog/activity/TimelineActivity.java
+++ trunk/src/com/micro4blog/activity/TimelineActivity.java
@@ -11,25 +11,34 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import com.micro4blog.GlobalFramework;
 import com.micro4blog.R;
 
-public class TimelineActivity extends GlobalFramework {
+public class TimelineActivity extends GlobalFramework implements OnClickListener {
 	
 	Activity mActivity;
+	LayoutParams mParams;
 	
+	FrameLayout mLayoutContent;
 	ListView mListView;
 	
 	LinearLayout mLayoutFooter;
-	
-	LayoutParams mParams;
-	
+	Button mFooterHome;
+	Button mFooterMessage;
+	Button mFooterProfile;
+	Button mFooterSquare;
+	Button mFooterMore;
+
 	boolean ib = true;
 	
 	public void onCreate(Bundle savedInstanceState) {
@@ -37,25 +46,22 @@ public class TimelineActivity extends GlobalFramework {
 		
 		mActivity = this;
 		
+		mParams = new LayoutParams(LayoutParams.FILL_PARENT, 
+				LayoutParams.WRAP_CONTENT);
+		
 		setContentView(R.layout.main_content);
 		
-//		mParams = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
-//		
-//		LayoutInflater footer = getLayoutInflater();
-//		mLayoutFooter = (LinearLayout) footer.inflate(R.layout.footer, null);
-//		
-//		mLayoutFooter.setGravity(Gravity.BOTTOM);
-//		
-//		getParent().addContentView(mLayoutFooter, mParams);
-		
-//		setUp();
-		
+		mLayoutContent = (FrameLayout) findViewById(R.id.content_main);
+				
 		mLayoutFooter = (LinearLayout) findViewById(R.id.footer);
 		mLayoutFooter.setVisibility(View.GONE);
 		
+		// 设置导航栏
+		setFooterUp();
+		
 	}
 
-	protected void setUp() {
+	protected void setListUp() {
 		mListView = (ListView) findViewById(R.id.list_main);
 		
 		ListAdapter adapter = new SimpleAdapter(mActivity, getMapData(), 
@@ -64,6 +70,8 @@ public class TimelineActivity extends GlobalFramework {
 						new int[] {R.id.username_textview, R.id.timeline_content, R.id.forwarding_content});
 		
 		mListView.setAdapter(adapter);
+		
+		
 		
 	}
 	
@@ -79,6 +87,70 @@ public class TimelineActivity extends GlobalFramework {
 		list.add(map);
 		
 		return list;
+	}
+	
+	protected void setFooterUp() {
+		mFooterHome = (Button) findViewById(R.id.footer_home);
+		mFooterMessage = (Button) findViewById(R.id.footer_message);
+		mFooterProfile = (Button) findViewById(R.id.footer_profile);
+		mFooterSquare = (Button) findViewById(R.id.footer_square);
+		mFooterMore = (Button) findViewById(R.id.footer_more);
+		
+		mFooterHome.setOnClickListener(this);
+		mFooterMessage.setOnClickListener(this);
+		mFooterProfile.setOnClickListener(this);
+		mFooterSquare.setOnClickListener(this);
+		mFooterMore.setOnClickListener(this);
+	
+	}
+	
+	@Override
+	public void onClick(View v) {
+
+		switch (v.getId()) {
+		case R.id.footer_home: {
+			mLayoutContent.removeAllViews();
+			
+			mLayoutContent.addView(mListView);
+			break;
+		}
+		case R.id.footer_message: {
+			mLayoutContent.removeAllViews();
+			
+			View view = getLayoutInflater().inflate(R.layout.gird_cell, null);
+			
+			mLayoutContent.addView(view);
+			
+			break;
+		}
+		case R.id.footer_profile: {
+			mLayoutContent.removeAllViews();
+			
+			View view = getLayoutInflater().inflate(R.layout.footer, null);
+			
+			mLayoutContent.addView(view);
+			break;
+		}
+		case R.id.footer_square: {
+			mLayoutContent.removeAllViews();
+			
+			View view = getLayoutInflater().inflate(R.layout.header, null);
+			
+			mLayoutContent.addView(view);
+			break;
+		}
+		case R.id.footer_more: {
+			mLayoutContent.removeAllViews();
+			
+			View view = getLayoutInflater().inflate(R.layout.list_item_timeline, null);
+			
+			mLayoutContent.addView(view);
+			break;
+		}
+		default :
+			break;
+		}
+		
 	}
 	
 	//================================================================
