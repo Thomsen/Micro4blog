ddd3604d8adc21b5092d7fecd3f0134db35cf566
androidfoross@gmail.com <androidfoross@gmail.com@ad5b3d4e-91d0-e1bb-e5e5-35cb20aa3a27>
2012-05-10 09:40:06 +0000

    notification test

--- trunk/src/com/micro4blog/tests/SwipeTest.java
+++ trunk/src/com/micro4blog/tests/SwipeTest.java
@@ -1,15 +1,23 @@
 package com.micro4blog.tests;
 
-import com.micro4blog.GlobalFramework;
-
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.GestureDetector.OnGestureListener;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import com.micro4blog.GlobalFramework;
+import com.micro4blog.Micro4blog;
+import com.micro4blog.R;
+import com.micro4blog.activity.HomeTimelineActivity;
+import com.micro4blog.server.Micro4blogForSina;
 
 public class SwipeTest extends Activity implements OnGestureListener {
 	public void onCreate(Bundle savedInstanceState) {
@@ -32,10 +40,13 @@ public class SwipeTest extends Activity implements OnGestureListener {
 		
 		// 触发不同的手势
 		view.setLongClickable(true);
+				
 	}
 	
 	public boolean onDown(MotionEvent event) {
 		Toast.makeText(this, "a", Toast.LENGTH_SHORT).show();
+		
+		notification();
 		
 		return false;
 	}
@@ -66,6 +77,30 @@ public class SwipeTest extends Activity implements OnGestureListener {
 		return false;
 	}
 
+	
+	// notification
+	private void notification() {
+		NotificationManager notificationManager = (NotificationManager) 
+				getSystemService(Context.NOTIFICATION_SERVICE);
+		
+		Notification notification = new Notification(R.drawable.ic_launcher, 
+				"new", System.currentTimeMillis());
+		
+		Context context = getApplicationContext();
+		
+//		Micro4blog micro4blog = Micro4blog.getInstance(context, Micro4blog.getCurrentServer());
+	
+		Micro4blog micro4blog = new Micro4blogForSina();
+		
+		Intent intent = new Intent(context, HomeTimelineActivity.class);
+		
+		PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+		
+		notification.setLatestEventInfo(context, "title", "text", pendingIntent);
+		
+		notificationManager.notify(1, notification);
+		
+	}
 
 	
 	
